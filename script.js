8aeffc3320d3ae15e6653d9e9c42334040cce88c
Amanda Santos <113608404+AmandaSoaress@users.noreply.github.com>
2024-01-25 12:25:10 -0300

    test

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const iniciarOuPausarBt = document.querySelector('#start-pause span')
-const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-butto-icon")
+const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-button-icon")
 const tempoNaTela = document.querySelector('#timer')
 
-const musica = new Audio('/sons/luna-rise-part-one.mp3')
-const audioPlay = new Audio('/sons/play.wav');
+const musica = new Audio('./sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('./sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
-const audioTempoFinalizado = new Audio('/sons/beep.mp3')
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 30
 let intervaloId = null
